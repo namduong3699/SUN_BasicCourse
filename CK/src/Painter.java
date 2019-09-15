d608044cbd24125f575a8fddde8cfe9ae4fda7d5
Dương Hoài Nam <33230044+namduong3699@users.noreply.github.com>
2019-09-15 21:34:51 +0700

    Add files via upload

---
+++ CK/src/Painter.java
@@ -0,0 +1,13 @@
+
+public class Painter {
+	private String painter;
+	Painter() {
+		painter = null;
+	}
+	public String getPainter() {
+		return painter;
+	}
+	public void setPainter(String painter) {
+		this.painter = painter;
+	}
+}
---
+++ CK/src/Shape.java
@@ -0,0 +1,6 @@
+
+public abstract class Shape {
+	private Color color;
+	private Painter painter;
+	public abstract void print();
+}
